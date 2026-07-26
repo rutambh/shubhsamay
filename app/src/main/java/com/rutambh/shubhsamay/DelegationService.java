815ecf2dc6a97e15835dd32d7bb568cd78420839
rutambh <rutambh@gmail.com>
2026-07-26 12:25:07 +0530

    feat(android): add initialized Android TWA wrapper project files for headless CI build

---
+++ app/src/main/java/com/rutambh/shubhsamay/DelegationService.java
@@ -0,0 +1,14 @@
+package com.rutambh.shubhsamay;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
